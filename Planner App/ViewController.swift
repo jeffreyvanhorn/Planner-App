88063fb600aaf902d44ee3f0be25e7b07878a3be
Jeffrey Van Horn <jeffreyvanhorn@Jeffreys-MacBook-Pro.local>
2018-10-21 16:58:23 -0700

    First Commit and Implemented the tab bar controller elements

---
+++ Planner App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Planner App
+//
+//  Created by Jeffrey Van Horn on 10/21/18.
+//  Copyright © 2018 Jeffrey Van Horn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
